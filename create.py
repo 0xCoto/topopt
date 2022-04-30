ebedb8e3ac4803d1ff2ef2f89f6cd86872c92c09
Apostolos <cotothearcher@gmail.com>
2022-04-30 22:46:18 +0300

    Windows version

--- create.py
+++ create.py
@@ -2,9 +2,13 @@ import time
 import random
 import string
 import os
-import sys
+#import sys
 # Set FreeCAD Python PATH
-sys.path = ['/usr/share/freecad/Mod/Web', '/usr/share/freecad/Mod/Raytracing', '/usr/share/freecad/Mod/Material', '/usr/share/freecad/Mod/Show', '/usr/share/freecad/Mod/Start', '/usr/share/freecad/Mod/Spreadsheet', '/usr/share/freecad/Mod/PartDesign', '/usr/share/freecad/Mod/Part', '/usr/share/freecad/Mod/Complete', '/usr/share/freecad/Mod/Path', '/usr/share/freecad/Mod/Image', '/usr/share/freecad/Mod/Import', '/usr/share/freecad/Mod/Robot', '/usr/share/freecad/Mod/Points', '/usr/share/freecad/Mod/Surface', '/usr/share/freecad/Mod/Test', '/usr/share/freecad/Mod/Tux', '/usr/share/freecad/Mod/Sketcher', '/usr/share/freecad/Mod/MeshPart', '/usr/share/freecad/Mod/Fem', '/usr/share/freecad/Mod/Measure', '/usr/share/freecad/Mod/Ship', '/usr/share/freecad/Mod/Drawing', '/usr/share/freecad/Mod/Plot', '/usr/share/freecad/Mod/Idf', '/usr/share/freecad/Mod/ReverseEngineering', '/usr/share/freecad/Mod/OpenSCAD', '/usr/share/freecad/Mod/Draft', '/usr/share/freecad/Mod/TechDraw', '/usr/share/freecad/Mod/Arch', '/usr/share/freecad/Mod/AddonManager', '/usr/share/freecad/Mod/Inspection', '/usr/share/freecad/Mod/Mesh', '/usr/share/freecad/Mod', '/usr/lib/freecad/lib64', '/usr/lib/freecad-python3/lib', '/usr/share/freecad/Ext', '/usr/lib/freecad/bin', '/usr/lib/python38.zip', '/usr/lib/python3.8', '/usr/lib/python3.8/lib-dynload', '/usr/local/lib/python3.8/dist-packages', '/usr/lib/python3/dist-packages', '', '/usr/lib/freecad/Macro']
+#sys.path = ['/usr/share/freecad/Mod/Web', '/usr/share/freecad/Mod/Raytracing', '/usr/share/freecad/Mod/Material', '/usr/share/freecad/Mod/Show', '/usr/share/freecad/Mod/Start', '/usr/share/freecad/Mod/Spreadsheet', '/usr/share/freecad/Mod/PartDesign', '/usr/share/freecad/Mod/Part', '/usr/share/freecad/Mod/Complete', '/usr/share/freecad/Mod/Path', '/usr/share/freecad/Mod/Image', '/usr/share/freecad/Mod/Import', '/usr/share/freecad/Mod/Robot', '/usr/share/freecad/Mod/Points', '/usr/share/freecad/Mod/Surface', '/usr/share/freecad/Mod/Test', '/usr/share/freecad/Mod/Tux', '/usr/share/freecad/Mod/Sketcher', '/usr/share/freecad/Mod/MeshPart', '/usr/share/freecad/Mod/Fem', '/usr/share/freecad/Mod/Measure', '/usr/share/freecad/Mod/Ship', '/usr/share/freecad/Mod/Drawing', '/usr/share/freecad/Mod/Plot', '/usr/share/freecad/Mod/Idf', '/usr/share/freecad/Mod/ReverseEngineering', '/usr/share/freecad/Mod/OpenSCAD', '/usr/share/freecad/Mod/Draft', '/usr/share/freecad/Mod/TechDraw', '/usr/share/freecad/Mod/Arch', '/usr/share/freecad/Mod/AddonManager', '/usr/share/freecad/Mod/Inspection', '/usr/share/freecad/Mod/Mesh', '/usr/share/freecad/Mod', '/usr/lib/freecad/lib64', '/usr/lib/freecad-python3/lib', '/usr/share/freecad/Ext', '/usr/lib/freecad/bin', '/usr/lib/python38.zip', '/usr/lib/python3.8', '/usr/lib/python3.8/lib-dynload', '/usr/local/lib/python3.8/dist-packages', '/usr/lib/python3/dist-packages', '', '/usr/lib/freecad/Macro']
+
+FREECADPATH = 'C:/Program Files/FreeCAD 0.19/bin'
+import sys
+sys.path.append(FREECADPATH)
 
 # Initiate GUI
 from PySide2 import QtCore, QtGui, QtWidgets
@@ -26,7 +30,7 @@ app.processEvents()
 app.processEvents()
 
 # New project
-exec(open('/usr/share/freecad/Mod/Start/StartPage/LoadNew.py').read())
+exec(open('C:/Program Files/FreeCAD 0.19/data/Mod/Start/StartPage/LoadNew.py').read())
 App.setActiveDocument("Unnamed")
 App.ActiveDocument=App.getDocument("Unnamed")
 Gui.ActiveDocument=Gui.getDocument("Unnamed")
@@ -101,6 +105,6 @@ app.processEvents()
 app.processEvents()
 
 import ImportGui
-ImportGui.export(__objs__,u"/home/abc/Desktop/topopt/all.step")
+ImportGui.export(__objs__,u"C:/Users/coto_/Desktop/topopt/all.step")
 
 del __objs__
